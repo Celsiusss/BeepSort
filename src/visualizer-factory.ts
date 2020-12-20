cf6a4252fa148a6013c25e698ab63dbbf466f8b6
Daniel Liland <danielsen2000@gmail.com>
2020-12-20 22:46:09 +0100

    circle visualizer

--- src/visualizer-factory.ts
+++ src/visualizer-factory.ts
@@ -2,6 +2,7 @@ import { Visualizer } from './visualizers/visualizer';
 import { StairsVisualizer } from './visualizers/stairs-visualizer';
 import { PyramidVisualizer } from './visualizers/pyramid-visualizer';
 import { DotsVisualizer } from './visualizers/dots-visualizer';
+import { CircleVisualizer } from './visualizers/circle-visualizer';
 
 export class VisualizerFactory {
     static visualizer(visualizer: Visualizers): Visualizer {
@@ -12,12 +13,14 @@ export class VisualizerFactory {
                 return new PyramidVisualizer();
             case 'dots':
                 return new DotsVisualizer();
+            case 'circle':
+                return new CircleVisualizer();
             default:
                 throw new Error(`${visualizer} not allowed`);
         }
     }
 }
 
-export const visualizers: Visualizers[] = ['stairs', 'pyramid', 'dots'];
+export const visualizers: Visualizers[] = ['stairs', 'pyramid', 'dots', 'circle'];
 
-export type Visualizers = 'stairs' | 'pyramid' | 'dots';
+export type Visualizers = 'stairs' | 'pyramid' | 'dots' | 'circle';
---
+++ src/visualizers/circle-visualizer.ts
@@ -0,0 +1,35 @@
+import { Visualizer } from './visualizer';
+import { AsyncListVisualizer } from '../async-list-visualizer';
+import { IControlsConfiguration } from '../models';
+import { hsl } from 'color-convert';
+
+export class CircleVisualizer implements Visualizer {
+    draw(
+        context: CanvasRenderingContext2D,
+        cWidth: number,
+        cHeight: number,
+        controls: IControlsConfiguration,
+        list: AsyncListVisualizer
+    ): void {
+        const hasColors = controls.colors;
+        context.fillStyle = '#000000';
+        context.fillRect(0, 0, cWidth, cHeight);
+        context.lineWidth = 1.5;
+        const center = [Math.floor(cWidth / 2), Math.floor(cHeight / 2)];
+        const radius = Math.min(center[0], center[1]);
+        let prevAngle = 0;
+        for (let i = 0; i < list.length; i++) {
+            const hue = 255 - (list.get(i) / list.length) * 360;
+            const color = '#' + (hasColors ? hsl.hex([hue, 100, 50]) : 'fff');
+            const angle = prevAngle + (1 / list.length) * Math.PI * 2;
+            context.fillStyle = color;
+            context.strokeStyle = color;
+            context.beginPath();
+            context.arc(center[0], center[1], radius, prevAngle, angle);
+            context.lineTo(center[0], center[1]);
+            context.fill();
+            context.stroke();
+            prevAngle = angle;
+        }
+    }
+}
