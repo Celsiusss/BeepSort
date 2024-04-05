fb69f9e3b4c7d9950afdc58a03f07294d3b0a8c3
Daniel Liland <celsiuss@await.sh>
2024-04-05 15:12:33 +0200

    remove webpack

--- webpack.analyze.js
+++
@@ -1,9 +0,0 @@
-const { merge } = require('webpack-merge');
-const development = require('./webpack.dev');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
-module.exports = merge(development, {
-    plugins: [
-        new BundleAnalyzerPlugin()
-    ]
-});
